89639c35b9f2e3ec23f07572fc378df35a64b3b7
BSSE24002 <bsse24002@itu.edu.pk>
2026-07-11 00:14:51 +0500

    push

--- FINAL/src/components/shop/Header.jsx
+++ FINAL/src/components/shop/Header.jsx
@@ -192,7 +192,7 @@ export function Header() {
               to="/"
               className="text-3xl font-sans font-black tracking-tighter uppercase relative group"
             >
-              <img src="/footer.png" alt="NEW LAB SYSTEM" className="h-10 animate-logo" />
+              <img src="/logo1.png" alt="NEW LAB SYSTEM" className="h-10 animate-logo" />
             </Link>
           </div>
 
